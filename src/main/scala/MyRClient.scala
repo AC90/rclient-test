7ae6d23889b1b14257a60c81207d6200234c73f9
Alex Curtis <alex.curtis@outlook.com>
2016-06-01 15:43:03 +0200

    add a date time stamp

--- src/main/scala/MyRClient.scala
+++ src/main/scala/MyRClient.scala
@@ -1,9 +1,10 @@
 
 import org.ddahl.rscala.callback.RClient
+import org.joda.time.DateTime
 
 class MyRClient () {
 	
-	System.out.println("Before instantiating RClient")
+	System.out.println("Before instantiating RClient : " + new DateTime().toString())
 	val Re = RClient()
-	System.out.println("After instantiating RClient")
+	System.out.println("After instantiating RClient : " + new DateTime().toString())
 }
